4e2ae498e7bcdf5540a8185a5ceb571c24ebb8d8
rimasultana <rimasultana20225@gmail.com>
2024-03-27 22:38:11 +0600

    setTimeOut

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-function myDisplayer(some){
-    console.log(some);
-}
-
-
-
-function myFirst(){
-    myDisplayer("Hello");
-}
-function mySeconsd(){
-    myDisplayer("Goodbye");
-}
-
-myFirst();
-mySeconsd();
-
-
-function disply(some){
-    console.log(some);
-}
-
-function claculator(num1, num2, myCallback){
+function myDisplayer(something) {
+    console.log(something);
+  }
+  
+  function myCalculator(num1, num2, myCallback) {
     let sum = num1 + num2;
-    myCallback (sum);
-}
+    myCallback(sum);
+  }
+  
+  myCalculator(5, 5, myDisplayer);
 
-let result = claculator(10, 10, disply);
+  setTimeout(myFunction, 3000);
+
+  function myFunction(){
+    console.log("I Love You!!!");
+  }
