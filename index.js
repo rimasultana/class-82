9f5d9dcbefc2ae5dc9ae0f6386d739c15bff82c3
rimasultana <rimasultana20225@gmail.com>
2024-03-27 22:01:01 +0600

    callback

---
+++ index.js
@@ -0,0 +1,15 @@
+function myDisplayer(some){
+    console.log(some);
+}
+
+
+
+function myFirst(){
+    myDisplayer("Hello");
+}
+function mySeconsd(){
+    myDisplayer("Goodbye");
+}
+
+myFirst();
+mySeconsd();
